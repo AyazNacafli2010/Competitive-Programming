8f2ddda7203c556e74043dc11a621e6ca268f9d0
AyazNacafli2010 <ayaznecefli12345@gmail.com>
2025-05-28 20:31:35 +0400

    dd

--- Topics/Bitmasks and Bit operations/e.cpp
+++ Topics/Bitmasks and Bit operations/e.cpp
@@ -1,47 +1,56 @@
 #include <bits/stdc++.h>
-#define isz(x) ((int)(x.size()))
+#define isz(x) (int)x.size()
 using namespace std;
 
-void remax(long long &a, long long b) { a = max(a, b); }
+constexpr int sz = 2e5 + 5;
+
+int a[sz], st[sz*4], n;
+
+void build(int l,int r,int v) {
+	if (l == r) {
+		st[v] = a[l];
+		return;
+	}
+	int mid = (l + r) / 2;
+	build(l, mid, v<<1);
+	build(mid+1, r, v<<1|1);
+	st[v] = st[v<<1]|st[v<<1|1];
+}
+int get(int l,int r,int a,int b,int v) {
+	if (l > b || a > r) return 0;
+	if (a <= l && r <= b) return st[v];
+	int mid = (l + r) / 2;
+	return get(l, mid, a, b, v<<1)|get(mid+1, r, a, b, v<<1|1);
+}
 void solve() {
-    int n, k; 
-	cin >> n >> k;
-    string a, b; 
-	cin >> a >> b;
-    set<char> s;
-    for (auto c : a)
-        s.insert(c);
-    vector<bool> ok(26, false);
-    vector<int> v;
-    for (char c : s)
-		v.push_back(c - 'a');
-    long long answ{};
-    for (int i = 0; i < (1 << isz(v)); i++) {
-        if (__builtin_popcount(i) > k)
-            continue;
-        fill(ok.begin(), ok.end(), false);
-        for (int j = 0; j < isz(v); j++) {
-            if ((1 << j) & i)
-                ok[v[j]] = true;
-		}
-        int last = 0;
-        long long res = 0;
-        for (int j = 0; j < n; j++) {
-            if (a[j] == b[j] || ok[a[j] - 'a'] == true)
-                res += j - last + 1;
-            else
-                last = j + 1;
-        }
-        remax(answ, res);
-    }
-    cout << answ << '\n';
+	cin >> n;
+	for (int i = 1; i <= n; i++) 
+		cin >> a[i];
+	build(1,n,1);
+	int low = 0, high = n + 1;
+	while (high - low > 1) {
+		int mid = (low + high) / 2;
+		auto check = [&]() {
+			set<int> s;
+			for (int i = 1; i <= n - mid + 1; i++)
+				s.insert(get(1, n, i, i + mid - 1,1));
+			if (isz(s) == 1) 
+				return true;
+			return false;
+		};
+		if (check())
+			high = mid;
+		else
+			low = mid;
+	}
+	cout << high << '\n';
     return;
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tt = 1; cin >> tt;
+    int tt; cin >> tt;
     while (tt --> 0) solve();
     return 0;
 }
--- Topics/Bitmasks and Bit operations/g.cpp
+++ Topics/Bitmasks and Bit operations/g.cpp
@@ -1,43 +1,47 @@
 #include <bits/stdc++.h>
-#define int long long
+#define isz(x) ((int)(x.size()))
 using namespace std;
 
-main() {
+void remax(long long &a, long long b) { a = max(a, b); }
+void solve() {
+    int n, k; 
+	cin >> n >> k;
+    string a, b; 
+	cin >> a >> b;
+    set<char> s;
+    for (auto c : a)
+        s.insert(c);
+    vector<bool> ok(26, false);
+    vector<int> v;
+    for (char c : s)
+		v.push_back(c - 'a');
+    long long answ{};
+    for (int i = 0; i < (1 << isz(v)); i++) {
+        if (__builtin_popcount(i) > k)
+            continue;
+        fill(ok.begin(), ok.end(), false);
+        for (int j = 0; j < isz(v); j++) {
+            if ((1 << j) & i)
+                ok[v[j]] = true;
+		}
+        int last = 0;
+        long long res = 0;
+        for (int j = 0; j < n; j++) {
+            if (a[j] == b[j] || ok[a[j] - 'a'] == true)
+                res += j - last + 1;
+            else
+                last = j + 1;
+        }
+        remax(answ, res);
+    }
+    cout << answ << '\n';
+    return;
+}
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, m; cin >> n >> m;
-	vector<int> a(n);
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
-	vector<int> v1, v2;
-	int part1 = n / 2;
-	for (int i = 0; i < (1 << part1); i++) {
-		int sum = 0;
-		for (int j = 0; j < n / 2; j++)
-			if ((1<<j)&i)
-				sum = (sum + a[j]) % m;
-		v1.push_back(sum);
-	}
-	int other = n - part1;
-	for (int i = 0; i < (1 << other); i++) {
-		int sum = 0;
-		for (int j = 0; j < other; j++)
-			if ((1<<j)&i)
-				sum = (sum + a[j+part1]) % m;
-		v2.push_back(sum);
-	}
-	sort(v2.begin(), v2.end());
-	int answ = 0;
-	for (auto &it : v1) {
-		int need = m - it;
-		auto i = lower_bound(v2.begin(),v2.end(),need);
-		if (i != v2.begin()) {
-			i--;
-			answ = max(answ, (*i + it) % m);
-		} else {
-			answ = max(answ, (v2.back() + it) % m);
-		}
-	}
-	cout << answ << '\n';
+
+    int tt = 1; cin >> tt;
+    while (tt --> 0) solve();
     return 0;
 }
