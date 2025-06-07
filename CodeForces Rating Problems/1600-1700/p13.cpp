c037710cc2f89599326c0c8c9e45438b8c884671
AyazNacafli2010 <ayaznecefli12345@gmail.com>
2025-06-07 17:03:19 +0400

    D. Arcology On Permafrost
    Add comments

--- CodeForces Rating Problems/1600-1700/p13.cpp
+++ CodeForces Rating Problems/1600-1700/p13.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 const int N = 5e5 + 5;
 int a[N];
-
+/**
+	1. n - m * k < n / (m + 1)
+	2. n - n / (m + 1) < m * k
+	3. n * (m + 1) - n / (m + 1) < m * k
+	4. n * m + n - n / (m + 1) < m * k
+	5. n * m / (m + 1) < m * k
+	6. n / (m + 1) < k
+	7. n < k * (m + 1) ========> n - m * k < k 
+	so a[i] = i mod k
+	in other condition a[i] = i mod (n / (m + 1))
+**/
 void solve() {
 	int n, m, k; cin >> n >> m >> k;
 	for (int i = 0; i <= n; i++)
